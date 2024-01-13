0d149c0c1de350caca6cd5b195cf7cce32a203b6
colton-fjeldos <156041809+colton-fjeldos@users.noreply.github.com>
2024-01-13 03:13:47 -0800

    Added options to StartMenu
    - Changed project initial scene to start menu.
    - Enabled FPS print to console every second (can be disabled in Project > Project Settings > Settings > "Print FPS"
    - "Options" button on main menu now works
    - Added options menu
    - - Added volume slider that prints to label
    - -  Label that displays current volume
    - - Back button that sends the player back to the main menu.
    
    All of these features exist on the same scene. I don't think that any of this stuff works inefficiently or lowers players framerates drastically by any means.

--- Scripts/MenuScript.cs
+++ Scripts/MenuScript.cs
@@ -6,11 +6,14 @@ public partial class MenuScript : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var options = GetNode<Godot.VBoxContainer>("MarginContainer/Options");
+		options.Hide();
 	}
 	
+	// MAIN MENU BUTTONS
 	private void _on_play_pressed()
 	{
-		GD.Print("Play button pressed");
+		GD.Print("Play button pressed, changing scenes");
 		GetTree().ChangeSceneToFile("res://Scenes/Scene1.tscn");
 	}
 
@@ -18,6 +21,10 @@ public partial class MenuScript : Control
 	private void _on_options_pressed()
 	{
 		GD.Print("Options button pressed");
+		var main = GetNode<Godot.VBoxContainer>("MarginContainer/Main");
+		var options = GetNode<Godot.VBoxContainer>("MarginContainer/Options");
+		main.Hide();
+		options.Show();
 		// Currently nonfunctional, you would disable the current containers
 		//and enable new containers that have the options menu
 	}
@@ -28,4 +35,25 @@ public partial class MenuScript : Control
 		GD.Print("Quit button pressed");
 		GetTree().Quit();
 	}
+	
+	//OPTION MENU BUTTONS/SLIDER
+	private void _on_back_pressed()
+	{
+		GD.Print("Back button pressed");
+		//I feel like calling GetNode might be expensive, I would probably only want to do this one time
+		//in "_Ready" function, but I don't know how to accomplish that. Same with VolumeLabel.
+		Godot.VBoxContainer main = GetNode<Godot.VBoxContainer>("MarginContainer/Main");
+		Godot.VBoxContainer options = GetNode<Godot.VBoxContainer>("MarginContainer/Options");
+		options.Hide();
+		main.Show();
+	}
+	
+	private void _on_volume_slider_value_changed(double value)
+	{
+		Godot.Label volumeLabel = GetNode<Godot.Label>("MarginContainer/Options/VolumeLabel");
+		volumeLabel.Text = "Volume: " + value;
+		//Currently also needs to control actual game volume, IDK how to control that yet.
+		GD.Print("Volume set to: " + value);
+	}
+	
 }
