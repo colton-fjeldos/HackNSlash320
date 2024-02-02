d229fa0bed5e6a1d8ce590c79c1cff1a85543c8b
colton-fjeldos <156041809+colton-fjeldos@users.noreply.github.com>
2024-02-02 12:50:05 -0800

    Create Exports For CameraController
    The values that control the camera can now be changed within the inspector instead of the script.

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -3,15 +3,29 @@ using System;
 
 public partial class CameraController : Camera2D
 {
+	[ExportGroup("Camera Script Components")]
+	[Export]
+	bool Moveable_Camera = true; //Whether or not this script will even run.
+	[Export(PropertyHint.Range, "1,100,")]
+	int Camera_Radius = 75; //Maximum distance from the player that the camera will move
+	[Export(PropertyHint.Range, "1,25000,")]
+	int Inverse_Camera_Speed = 25000; //The larger this number is, the slower the camera accelerates
+	
 	Godot.Viewport screen;
+	
+	
+	
+	
 	public override void _Ready()
-	{
-		screen = GetViewport();
+	{	
+		if (Moveable_Camera) screen = GetViewport();
 	}
 
 	public override void _Process(double delta)
 	{	
-		Vector2 mouse_coor = screen.GetMousePosition() - screen.GetVisibleRect().Size / 2;
-		Position = Position.Lerp(mouse_coor.Normalized() * 75, mouse_coor.Length() / 5000);
+		if (Moveable_Camera){
+			Vector2 mouse_coor = screen.GetMousePosition() - screen.GetVisibleRect().Size / 2;
+			Position = Position.Lerp(mouse_coor.Normalized() * Camera_Radius, mouse_coor.Length() / Inverse_Camera_Speed);
+		}
 	}
 }
