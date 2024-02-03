dd7649f065a3b92c069d1a478f42ddbe3ac76b05
David Bratkov <30717692+David-Bratkov@users.noreply.github.com>
2024-02-02 23:32:08 -0800

    removed a scene and changed the script
    i removed the pause menu scene and put the pause menu inside scene1 and I am planning on having the visibility toggle work

--- Scripts/Scene1.cs
+++ Scripts/Scene1.cs
@@ -1,21 +1,23 @@
 using Godot;
 using System;
 
-public partial class Scene1 : Control
+public partial class Scene1 : Node2D
 {
-	
-	
+	Godot.VBoxContainer pausemenu;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		pausemenu = GetNode<Godot.VBoxContainer>("MarginContainer/PauseMenu");
+		pausemenu.Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		if (Input.IsActionPressed("ui_cancel"))
+		{
+			GD.Print("Read esc input");
+			//flip the visibility of the ui here with show and hide
+		}
 	}
-	//Opening up the esc menu when pressed esc or going back a menu
-	
-	
 }
