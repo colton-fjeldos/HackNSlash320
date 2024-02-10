58295a66cecb00c2686aea45c1e085dd7703dfe6
Kyle L <kyle.lumague@hotmail.com>
2024-02-09 22:51:34 -0800

    Issues with slime AI
    Moved slime

---
+++ Scripts/slime.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 100
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var player = get_node("/root/Node2D/controlled1")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	position += (player.position - position)/SPEED
+
+	move_and_slide()
+
