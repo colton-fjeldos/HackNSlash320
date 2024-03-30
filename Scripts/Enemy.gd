fe080cbd87ada9f93595e8f6751c5f1957f3458b
Sharon2G <sharongracek2004@gmail.com>
2024-03-29 23:43:35 -0700

    Added connections

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,37 +1,32 @@
 extends CharacterBody2D
 
-var _SPEED = 60
+
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var _leftRay
-var _rightRay
+var _walkPoint = preload("res://Scenes/walkpoints.tscn")
 var _player
-var _animation
 var _health = 100
 var _inChase = false
-var _enemyMovement
 var _pathfinder
 
-
-# State variables
 enum States {idle, patrolling, chasing, attacking}
-var state
+var state = States.patrolling
 
 func _ready():
-	_leftRay = $LeftRaycast
-	_rightRay = $RightRaycast
+	
+	_stateUpdate()
 	_player = get_parent().get_node("controlled1")
-	state = States.patrolling 
-	_animation = $AnimationPlayer
 	_pathfinder = get_parent().get_node("Pathfinder")
 
 func _physics_process(delta):
 	
+
 	if(!is_on_floor()):
 		velocity.y += _gravity * delta
 		move_and_slide()
 	
 	if(global_position.y > 630):
 		queue_free()
+
 	
 	if (_inChase == false):
 		if (_playerInChaseRange(global_position, _player.global_position)):
@@ -44,95 +39,116 @@ func _physics_process(delta):
 		if (_playerInAttackRange(global_position, _player.global_position)):
 			_attack()
 		else:
+			state = States.chasing
+			_stateUpdate()
 			_chase()
 
+
+func _stateUpdate():
+	if(state == States.patrolling):
+		$Attack.visible = false
+		$Walk.visible = true
+		$Run.visible = false
+		$AnimationPlayer.play("Walk")
+		
+	elif (state == States.chasing):
+		$Attack.visible = false
+		$Walk.visible = false
+		$Run.visible = true
+		$AnimationPlayer.play("Run")
+	
+	else:
+		$Walk.visible = false
+		$Run.visible = false
+		$Attack.visible = true
+		$AnimationPlayer.play("Attack")
+		
+
 func _patrol():
-	_SPEED = 30
-	$Attack.visible = false
-	$Walk.visible = true
-	$Run.visible = false
-	$AnimationPlayer.play("Walk")
+	var patrolSpeed = 30
 
 	if (!$LeftRaycast.is_colliding() or !$RightRaycast.is_colliding()):
 		if ($Walk.flip_h == true):
 			$Walk.flip_h = false
-			velocity.x = _SPEED
+			velocity.x = patrolSpeed
 		else:
 			$Walk.flip_h = true
-			velocity.x = -1 * _SPEED
+			velocity.x = -1 * patrolSpeed
 	move_and_slide()
 
 func _chase():
-	_SPEED = 70
-	$Attack.visible = false
-	$Walk.visible = false
-	$Run.visible = true
-	$AnimationPlayer.play("Run")
+	var chaseSpeed = 70
 	var jumpForce = -200
-	var isJumping
-	var reachPoint = -1
 	
 	
-	_enemyMovement = _pathfinder.getPath(global_position, _player.global_position)
+	var _enemyMovement = _pathfinder.getPath(global_position, _player.global_position)
 	_enemyMovement.push_back(_player.global_position)
 	var nextPos = _enemyMovement[0]
 	#print (_enemyMovement)
 	#print ("nextPos: ", nextPos, " enemyPos: ", global_position, "vel: ", velocity.x)
-	#print()
+	#print("nextPos: ", nextPos)
 	#print ("playerPos: ", _player.global_position)
+	#print(" enemyPos: ", global_position)
 	
 	if(_enemyMovement.size() > 1):
-		if _enemyMovement[0].y == _enemyMovement[1].y or _enemyMovement.size() == 2 :
+		if _enemyMovement[0].y == _enemyMovement[1].y or _enemyMovement.size() == 2:
 			if (abs(_enemyMovement[0].x - global_position.x) < abs(_enemyMovement[0].x - _enemyMovement[1].x)):
 				nextPos = _enemyMovement[1]
-
-
-	if (nextPos.x <= global_position.x + 2):
+				print ("nextPos: ", nextPos, " enemyPos: ", global_position)
+				
+		#if (abs(_enemyMovement[0].x - global_position.x) < 4):
+				#nextPos = _enemyMovement[1]
+				#print ("nextPos: ", nextPos, " enemyPos: ", global_position)
+				
+			
+				
+	
+				
+	if (nextPos.x <= global_position.x):
 		$Run.flip_h = true
-		velocity.x = -1 * _SPEED
-		if !_leftRay.is_colliding() and is_on_floor():
+		velocity.x = -1 * chaseSpeed
+		if !$LeftRaycast.is_colliding() and is_on_floor():
 			velocity.y = jumpForce
+			
 
 	else:
 		$Run.flip_h = false
-		velocity.x = _SPEED
+		velocity.x = chaseSpeed
 		
-		if !_rightRay.is_colliding() and is_on_floor():
+		if !$RightRaycast.is_colliding() and is_on_floor():
 			velocity.y = jumpForce
 	
-	
-	if ((nextPos.y + 8) < (global_position.y) and _player.is_on_floor() and is_on_floor()):
-		var y = (global_position.y - nextPos.y) * jumpForce
-		velocity.y = jumpForce
+		
+	#if ((nextPos.y + 8) < (global_position.y) and _player.is_on_floor() and is_on_floor()):
+		#var y = (global_position.y - nextPos.y) * jumpForce
+		#velocity.y = jumpForce
 	
 	move_and_slide()
 
 
 func _attack():
-	$Walk.visible = false
-	$Run.visible = false
-	$Attack.visible = true
 	
 	if (_player.global_position.x <= global_position.x):
 		$Attack.flip_h = 	true
 	else:
 		$Attack.flip_h = false
-	$AnimationPlayer.play("Attack")
 	
 	velocity = Vector2.ZERO
 	move_and_slide()
 
 
 func _playerInChaseRange(enemyPos, playerPos):
-	if (enemyPos.distance_to(playerPos) < 100 and enemyPos.distance_to(playerPos) > 40):
+	if (enemyPos.distance_to(playerPos) < 100 and enemyPos.distance_to(playerPos) > 10):
 		state = States.chasing
+		_stateUpdate()
 		return true
 	return false
 
 
 func _playerInAttackRange(enemyPos, playerPos):
-	if (enemyPos.distance_to(playerPos) < 40):
+	if (enemyPos.distance_to(playerPos) < 10):
 		state = States.attacking
+		_stateUpdate()
 		return true
 	return false
 
