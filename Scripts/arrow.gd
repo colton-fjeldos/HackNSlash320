29154abf589d4b4a174440d85fcefc238c293dc7
Kyle L <kyle.lumague@hotmail.com>
2024-04-05 23:45:23 -0700

    Adding a projectile script
    Added placeholder arrow scene with script that can be used for both player and AI.

---
+++ Scripts/arrow.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+var speed = 300
+
+func _ready():
+	set_as_top_level(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
