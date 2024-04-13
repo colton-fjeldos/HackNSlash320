fcd74c903898f648bbe25c1246508a840023f7e0
Sharon2G <sharongracek2004@gmail.com>
2024-04-12 23:48:35 -0700

    Random Rooms

--- Scripts/randomLevel.gd
+++ Scripts/randomLevel.gd
@@ -1,22 +1,40 @@
 extends Node2D
 
-var LR = preload("res://Scenes/Rooms/LR room.tscn")
-var LRB = preload("res://Scenes/Rooms/LRB room.tscn")
-var LRT = preload("res://Scenes/Rooms/LRT room.tscn")
-var LRBT = preload("res://Scenes/Rooms/LRBT room.tscn")
-var rooms = [LR, LRB, LRT, LRBT]
-
-func _ready():
-	var startNode = get_node("StartPositions")
-	var startPositions = startNode.get_children()
-	var startingPos = []
-
+var directions = [Vector2.DOWN, Vector2.UP, Vector2.RIGHT, Vector2.LEFT]
 	
-	for i in range (len(startPositions)):
-		startingPos.push_back(startPositions[i].global_position)
-	
-	var startPoint = _getRandomPt(startingPos)
+func _createWalk(startPos):
+	var path
+	var currentPos 
+	path.push_back(startPos)
+	currentPos = startPos
+
+	for i in range(10):
+		var direction = _getRandomPt(directions)
+		var nextPos = currentPos + direction
+		path.push_back(nextPos)
+		currentPos = nextPos
+	return path
+
 	
 func _getRandomPt(array):
 	var index = randi() % array.size() 
 	return array[index]
+	
+func _createRooms():
+	var numOfRooms
+	var currentPos = Vector2.ZERO;
+	var mapPoints
+	for ii in range (10):
+		var path = _createWalk(currentPos)
+		mapPoints = mapPoints + path
+		currentPos = _getRandomPt(mapPoints)
+	
+	var tilemap = get_parent().get_node("TileMap")
+	var tilePositions 
+	for point in mapPoints:
+		for direction in directions:
+			var neighbour = position + direction;
+			if (neighbour in mapPoints):
+				tilePositions.push_back(neighbour)
+				tilemap.set_cell(0, tilemap.local_to_map(neighbour))
+
